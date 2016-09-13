05c0b4eab09c6ffd42dd84e8d3fb0e852c6fff6d
Jelani Thompson <jelanithompson@Jelanis-MacBook-Pro.local>
2016-09-12 21:53:24 -0400

    Starting to build layout

--- scripts/sound.js
+++ scripts/sound.js
@@ -7,18 +7,21 @@ var rightEar = new Tone.Oscillator().connect(merge.right);
 leftEar.frequency.value = 400;
 rightEar.frequency.value = 500;
 
-//Start frequencies in each ear
+// //Start frequencies in each ear
 // leftEar.start()
 //        .stop("5s");
 //
 // rightEar.start()
 //         .stop("10s");
 
+var leftSlide = $("#left");
+var rightSlider = $("#right");
+
 //Frequency is equivalent to difference between frequency in left and right ear
 var frequency = {
-  "Gamma" : [30, 50],
-  "Beta" : [14, 30],
-  "Alpha" : [8, 14],
-  "Theta" : [4, 8],
-  "Delta" : [0.1, 4]
+    "Gamma": [30, 50],
+    "Beta": [14, 30],
+    "Alpha": [8, 14],
+    "Theta": [4, 8],
+    "Delta": [0.1, 4]
 };
